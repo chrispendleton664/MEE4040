f567ff2628f55540295da588207dd45783e7adce
chrispendleton664 <75120467+chrispendleton664@users.noreply.github.com>
2020-12-23 18:38:57 +0000

    Delete inputNumber.m

--- inputNumber.m
+++
@@ -1,8 +0,0 @@
-function out = inputNumber(prompt)
-out = NaN;
-tot = 3; % how many attempts to make
-while isnan(out) && tot>0
-    out = str2double(input('Write your age :', 's'));
-    tot = tot-1;
-end
-end
